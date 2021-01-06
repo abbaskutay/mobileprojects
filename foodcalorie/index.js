28bbac3b27f5c4de99b5b663c5dc619036523033
abbaskutay <abbaskutaymedine@gmail.com>
2021-01-06 21:21:35 +0300

    main

---
+++ foodcalorie/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import AppContainer from './assets/config/stack';
+
+
+AppRegistry.registerComponent(appName, () => AppContainer);
